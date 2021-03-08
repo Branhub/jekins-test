68162aaa97f58ae5352cef2ed3b3240b0fe4b1cc
Branhub <fkqah64@126.com>
2021-03-08 16:52:47 +0800

    加入一个空白服务，用来测试启动失败的情况；测试页面版本+1；等等

--- src/main/java/net/feng/jenkinstest/controller/TestController.java
+++ src/main/java/net/feng/jenkinstest/controller/TestController.java
@@ -1,5 +1,7 @@
 package net.feng.jenkinstest.controller;
 
+import net.feng.jenkinstest.service.interfaces.IBlankService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController
 {
-    @RequestMapping(method = RequestMethod.GET,value = "/testPage")
-    public String toTestPage() {return "testPage.html";}
+
+//    @Autowired
+//    IBlankService blankService;
+
+    @RequestMapping(method = RequestMethod.GET,value = "/toTestPage")
+    public String toTestPage() {return "testPage";}
 }
---
+++ src/main/java/net/feng/jenkinstest/service/interfaces/IBlankService.java
@@ -0,0 +1,5 @@
+package net.feng.jenkinstest.service.interfaces;
+
+public interface IBlankService
+{
+}
